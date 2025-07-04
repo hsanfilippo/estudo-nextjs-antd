1af08fe3dcf38317a32e73adc835399d5e811db1
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-07-04 14:02:04 -0300

    feat: criando requisicao GET à /api/listarContatos

--- src/components/CardListagem/index.tsx
+++ src/components/CardListagem/index.tsx
@@ -1,15 +1,42 @@
+import { useEffect } from 'react'
 import styles from './styles.module.css'
 
-type Props = {
-  title: string
-  content: {}
-}
-
 export function CardListagem() {
+  
+  const getContatos = async () => {
+    try {
+      const res = await fetch('api/listarContatos', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+        }
+      })
+
+      if (!res.ok) throw new Error("Erro ao buscar por contatos")
+
+      const contatosData = await res.json()
+      console.log("Contatos:", contatosData)
+
+    } catch (erro) {
+      console.error("Erro ao carregar contatos:", erro)
+    }
+  }
+
+  useEffect(() => {
+    getContatos()
+  }, [])
+
   return (
     <div className={styles.card}>
       <div className={styles.cardTitle}>
         <h3>Titulo do card</h3>
+        <div>
+          <p>Nome aqui</p>
+          <p>Tel aqui</p>
+        </div>
+        <div>
+          <p>Categoria aqui</p>
+        </div>
       </div>
     </div>
   )
