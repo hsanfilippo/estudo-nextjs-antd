4a820299bc8f137848d6d6b3573c4a55d2760770
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-07-04 13:42:44 -0300

    feat: substituindo Header pelo novo metodo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css"
 import { Formulario } from "@/components/Formulario";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
@@ -9,9 +9,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles.header}>
-        <h1 className="container">Form com Ant Design</h1>
-      </header>
+      <Header headerContent="Adicionar um contato" />
       <Formulario />
     </>
   );
